98bad62f21b1a3ee96c73575287684ce40f3f838
Rhythm Dubey <rhythm@itradicals.com>
2025-09-24 00:25:45 +0530

    Implemented facade

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
+use App\Parcel;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('facade', function () {
+    Parcel::send('Book', 'Medium', 'First Facade', 'rhythm@itradicals.com');
+});
+
+Route::get('parcel', function() {
+    $deliveryService = new App\DeliveryService('India', 'Speed Post');
+
+    $deliveryService->send(
+        'rhythm@itradicals.com',
+        'Hello Rhythm',
+        'Book',
+        '16x17x18'
+    );
 });
 
 Route::get('process', [\App\Http\Controllers\PaymentGatewayController::class, 'process']);
